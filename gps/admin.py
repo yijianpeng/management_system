0a6f38f3fa92e2f0fcdfd540eb08946c5819ca9c
yijianpeng <2531942094@qq.com>
2023-03-26 21:01:21 +0800

    派单

--- gps/admin.py
+++ gps/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Location
 # Register your models here.
 admin.site.site_header = '车载巡检系统管理后台'  # 设置header
@@ -7,4 +6,30 @@ admin.site.site_title = '车载巡检系统管理后台'   # 设置title
 admin.site.index_title = '车载巡检系统管理后台'
 
 
-admin.site.register(Location)
+
+class Location_Admin(admin.ModelAdmin):
+    list_display = ('id','longitude', 'latitude')
+   
+    # 增加自定义按钮
+    actions = ['make_copy', 'custom_button']
+
+    def custom_button(self, request, queryset):
+        pass
+
+    # 显示的文本，与django admin一致
+    custom_button.short_description = '测试按钮'
+    # icon，参考element-ui icon与https://fontawesome.com
+    custom_button.icon = 'fas fa-audio-description'
+
+    # 指定element-ui的按钮类型，参考https://element.eleme.cn/#/zh-CN/component/button
+    custom_button.type = 'danger'
+
+    # 给按钮追加自定义的颜色
+    custom_button.style = 'color:black;'
+
+    def make_copy(self, request, queryset):
+        pass
+    make_copy.short_description = '复制员工'
+
+
+admin.site.register(Location, Location_Admin)
--- system/urls.py
+++ system/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 import log
 from log import urls
+
+from django.urls import re_path
+from django.views.static import serve
+from .settings import MEDIA_ROOT
 urlpatterns = [
    path("admin/", admin.site.urls),
    path('',include('log.urls')),
    path('',include('gps.urls')),
+   re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
